c1d48aedc5f8578f45db82bd1fef19190450d7b3
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-10-25 20:32:26 +0630

    Add creating table query

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE
+    animals (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        date_of_birth DATE NOT NULL,
+        escape_attempts INT NOT NULL,
+        neutered BOOLEAN,
+        weight_kg DECIMAL(5, 2) NOT NULL
+    );
