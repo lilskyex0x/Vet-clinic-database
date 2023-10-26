a19e29f059ef3bd5e5a0ed23c5eabc32690acf01
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-10-26 23:06:39 +0630

    Added owner data and species data

--- data.sql
+++ data.sql
@@ -29,3 +29,59 @@ VALUES
     ('Boarmon', '2005-06-07', 20.4, true, 7),
     ('Blossom', '1998-10-13', 17, true, 3),
     ('Ditto', '2022-05-14', 22, true, 4);
+
+INSERT INTO
+    owners (full_name, age)
+VALUES
+    ('San Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+INSERT INTO
+    species (name)
+VALUES
+    ('Pokemon'),
+    ('Digimon')
+
+-- Update the species_id based on the name of the animal
+BEGIN;
+
+UPDATE animals
+SET species_id = CASE
+    WHEN name ILIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END;
+
+-- Commit the transaction
+COMMIT;
+
+SELECT * FROM animals;
+
+-- Update the owner_id based on the owner's name and the animal's name
+UPDATE animals
+SET owner_id = (
+    SELECT id FROM owners WHERE full_name = 'Sam Smith'
+) WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (
+    SELECT id FROM owners WHERE full_name = 'Jennifer Orwell'
+) WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = (
+    SELECT id FROM owners WHERE full_name = 'Bob'
+) WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = (
+    SELECT id FROM owners WHERE full_name = 'Melody Pond'
+) WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (
+    SELECT id FROM owners WHERE full_name = 'Dean Winchester'
+) WHERE name IN ('Angemon', 'Boarmon');
