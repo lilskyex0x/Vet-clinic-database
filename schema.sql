4a6e0bd6c2389545f26c4346320d4ef2c0b5bfc9
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-10-27 22:25:49 +0630

    Create table for vets and specializations

--- schema.sql
+++ schema.sql
@@ -21,3 +21,24 @@ ADD COLUMN species_id INTEGER REFERENCES species (id);
 -- Add the "owner_id" column as a foreign key referencing the "owners" table
 ALTER TABLE animals
 ADD COLUMN owner_id INTEGER REFERENCES owners (id);
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation DATE
+);
+CREATE TABLE specializations (
+    vet_id INTEGER,
+    species_id INTEGER,
+    PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id),
+    FOREIGN KEY (species_id) REFERENCES species (id)
+);
+CREATE TABLE visits (
+    visit_id SERIAL PRIMARY KEY,
+    animal_id INTEGER,
+    vet_id INTEGER,
+    visit_date DATE,
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
